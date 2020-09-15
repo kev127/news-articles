31be2894683d52fbedac2f365fbe34abda0db8d3
kev127 <kelvinkeya125@gmail.com>
2020-09-15 19:16:52 +0300

    update config.py file

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-import os
-
 class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_KEY= os.environ.get('NEWS_API_KEY')
-    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
-    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
+    pass
+
+
 
 class ProdConfig(Config):
     '''
     Production  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
@@ -20,13 +19,9 @@ class ProdConfig(Config):
 class DevConfig(Config):
     '''
     Development  configuration child class
+
     Args:
         Config: The parent configuration class with General configuration settings
     '''
 
     DEBUG = True
-
-config_options = {
-'development':DevConfig,
-'production':ProdConfig
-}
